0302eb3c987a7d0043c6008c1abca0bbfac71e2f
realstealthninja <realstealthninja@gmail.com>
2022-09-30 20:03:28 +0530

    lint: lint run

--- main.py
+++ main.py
@@ -3,10 +3,10 @@ from src import Glykon
 
 if __name__ == "__main__":
 
-    if not ".env" in os.listdir():
-        
+    if ".env" not in os.listdir():
+
         print("⇒ Running configurations")
-       
+
         token = input("🔴 paste your bot's token 🔴:")
         with open(".env", "x") as f:
             f.write(f"TOKEN='{token}'")
--- src/bot.py
+++ src/bot.py
@@ -1,43 +1,49 @@
+"""
+    Main bot file
+"""
 import os
 from disnake import Intents
-from disnake.ext.commands import (
-    Bot
-)
+from disnake.ext.commands import Bot
 
 from dotenv import load_dotenv
+
 load_dotenv(dotenv_path="./.env")
-TOKEN=os.getenv("TOKEN")
+TOKEN = os.getenv("TOKEN")
+
 
 class Glykon(Bot):
+    """
+    Bots class
+    """
     def __init__(self):
         super().__init__(
             command_prefix="changeme",
             case_insensitive=True,
-            help_command= None, # type: ignore
-            intents=Intents.all()
+            help_command=None,  # type: ignore
+            intents=Intents.all(),
         )
 
     def loadCogs(self) -> None:
         for file in os.listdir("./src/cogs"):
-            if file.startswith("_"): continue
-            if not file.endswith("py"): self.load_extension(f"src.cogs.{file}")
+            if file.startswith("_"):
+                continue
+            if not file.endswith("py"):
+                self.load_extension(f"src.cogs.{file}")
             self.load_extension(f"src.cogs.{file[:-3]}")
 
     async def on_ready(self) -> None:
         print(
             f"""
-                ───── ⋆⋅☆⋅⋆ ─────
-                WELCOME TO {self.user.display_name}
-                ⤷ Servers 💿: {len(self.guilds)} 
-                ⤷ Users 👥: {len(self.users)} 
-                ⤷ Cogs ⚙️: {len(self.cogs)} 
-                ───── ⋆⋅☆⋅⋆ ─────
-
-                Status: Ready 🟢
+            ───── ⋆⋅☆⋅⋆ ─────
+            WELCOME TO {self.user.display_name}
+            ⤷ Servers 💿: {len(self.guilds)}
+            ⤷ Users 👥: {len(self.users)}
+            ⤷ Cogs ⚙️: {len(self.cogs)}
+            ───── ⋆⋅☆⋅⋆ ─────
+                🟢 Ready 🟢
             """
         )
 
-    
     def run(self) -> None:
         self.loadCogs()
         super().run(TOKEN, reconnect=True)
