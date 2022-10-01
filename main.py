b3620133e3255522e7d8c7a59596c511a7208882
realstealthninja <68815218+realstealthninja@users.noreply.github.com>
2022-10-01 18:53:09 +0530

    Revert asking user for token

--- main.py
+++ main.py
@@ -2,11 +2,4 @@ import os
 from src import Glykon
 
 if __name__ == "__main__":
-    if ".env" not in os.listdir():
-        print("⇒ Running configurations")
-
-        token = input("🔴 paste your bot's token 🔴:")
-        with open(".env", "x") as f:
-            f.write(f"TOKEN='{token}'")
-
     Glykon().run()
