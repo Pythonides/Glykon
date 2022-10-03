537236580dda4eccb744e1863746798fb1e4c692
realstealthninja <68815218+realstealthninja@users.noreply.github.com>
2022-10-03 18:48:11 +0530

    Update bot.py

--- src/bot.py
+++ src/bot.py
@@ -37,11 +37,7 @@ class Glykon(Bot):
     def load_cogs(self) -> None:
         """Load cogs from the src/cogs directory"""
         for file in os.listdir("./src/cogs"):
-            if file.startswith("_"):
-                continue
-            if not file.endswith(".py"):
-                self.load_extension(f"src.cogs.{file}")
-            self.load_extension(f"src.cogs.{file[:-3]}")
+            self.load_extensions("./src/cogs")
 
     async def on_ready(self) -> None:
         """On bot ready"""
